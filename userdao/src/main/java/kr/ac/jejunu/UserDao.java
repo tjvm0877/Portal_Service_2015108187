871a13f364592bdac2234cb1cf06e23fbbdd9d77
Hyun <itsmine096@gmail.com>
2021-03-26 18:10:08 +0900

    3/26실습 내용

--- userdao/src/main/java/kr/ac/jejunu/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/UserDao.java
@@ -3,14 +3,14 @@ package kr.ac.jejunu;
 import java.sql.*;
 
 public class UserDao {
+    private final ConnectionMaker connectionMaker;
+    public UserDao(ConnectionMaker connectionMaker){
+        this.connectionMaker = connectionMaker;
+    }
+
     public User findById(Integer id) throws ClassNotFoundException, SQLException {
         //데이터 어딨어? => mysql
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection =
-                DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/potal_service?" + "characterEncoding=utf-8&serverTimezone=UTC"
-                        ,"jeju","1234"
-                );
+        Connection connection = connectionMaker.getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement(
                 "select * from  userinfo where id = ?"
         );
@@ -26,6 +26,27 @@ public class UserDao {
         connection.close();
         return user;
     }
+
+    public void insert(User user) throws ClassNotFoundException, SQLException {
+        Connection connection = connectionMaker.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "insert into userinfo (name, password) values ( ?, ? )"
+                , Statement.RETURN_GENERATED_KEYS
+        );
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+
+        preparedStatement.executeUpdate();
+
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        resultSet.next();
+
+        user.setId(resultSet.getInt(1));
+
+        preparedStatement.close();
+        connection.close();
+    }
+
 }
 
 
--- userdao/src/test/java/kr/ac/jejunu/UserDaoTests.java
+++ userdao/src/test/java/kr/ac/jejunu/UserDaoTests.java
@@ -5,19 +5,80 @@ import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 
 public class UserDaoTests {
     @Test
     public void get() throws SQLException, ClassNotFoundException {
         Integer id = 1;
-        String name = "hyun";
+        String name = "재";
         String password = "1234";
 
-        UserDao userDao = new UserDao();
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao = daoFactory.getUserDao();
+
         User user = userDao.findById(id);
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
         assertThat(user.getPassword(), is(password));
     }
+
+    @Test
+    public void insert() throws SQLException, ClassNotFoundException {
+        String name = "Hyun";
+        String password = "1234";
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao = daoFactory.getUserDao();
+
+        userDao.insert(user);
+
+        User insertedUser = userDao.findById(user.getId());
+
+        assertThat(user.getId(), greaterThan(0));
+        assertThat(user.getId(), is (user.getId()));
+        assertThat(insertedUser.getName(), is(user.getName()));
+        assertThat(insertedUser.getPassword(), is(user.getPassword()));
+    }
+//
+//    @Test
+//    public void getHalla() throws SQLException, ClassNotFoundException {
+//        Integer id = 1;
+//        String name = "재현";
+//        String password = "1234";
+//
+//        UserDao userDao = new UserDao(new HanlaConnectionMaker());
+//        User user = userDao.findById(id);
+//        assertThat(user.getId(), is(id));
+//        assertThat(user.getName(), is(name));
+//        assertThat(user.getPassword(), is(password));
+//    }
+//
+//    @Test
+//    public void insertHalla() throws SQLException, ClassNotFoundException {
+//        String name = "재현";
+//        String password = "1111";
+//
+//        User user = new User();
+//        user.setName(name);
+//        user.setPassword(password);
+//        UserDao userDao = new UserDao(new HanlaConnectionMaker());
+//        userDao.insert(user);
+//
+//        User insertedUser = userDao.findById(user.getId());
+//
+//        assertThat(user.getId(), greaterThan(0));
+//        assertThat(insertedUser.getId(), is(user.getId()));
+//        assertThat(insertedUser.getName(), is(user.getName()));
+//        assertThat(insertedUser.getPassword(), is(user.getPassword()));
+//    }
+//
+
+
+
 }
+
