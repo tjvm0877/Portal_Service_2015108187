cb6d09d4c1dff6eb217d5c6870db4a304b882a63
Mr. Remark <itsmine096@gmail.com>
2021-05-15 05:52:47 +0900

    Connection Information : ConnectionMaker => DataSource

--- User_Dao/src/main/java/kr/ac/jejunu/DaoFactory.java
+++ User_Dao/src/main/java/kr/ac/jejunu/DaoFactory.java
@@ -1,17 +1,50 @@
 package kr.ac.jejunu;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
 
 @Configuration
 public class DaoFactory {
+    @Value("${db.classname}")
+    private String driver;
+
+    @Value("${db.username}")
+    private String username;
+
+    @Value("${db.password}")
+    private String password;
+
+    @Value("${db.url}")
+    private String url;
+
     @Bean
-    public UserDao userDao() {
-        return new UserDao(connectionMaker());
+    public UserDao userDao() throws ClassNotFoundException {
+        return new UserDao(dataSource());
     }
 
     @Bean
-    public JejuConnectionMaker connectionMaker() {
-        return new JejuConnectionMaker();
+    public DataSource dataSource() throws ClassNotFoundException {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass((Class<? extends Driver>) Class.forName(driver));
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setUrl(url);
+        return dataSource;
     }
 }
+
+/*
+    public Connection getConnection() throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/portal_service?" +
+                        "characterEncoding=utf-8&serverTimezone=UTC"
+                , "jeju", "1234"
+        );
+    }
+ */
--- User_Dao/src/main/java/kr/ac/jejunu/UserDao.java
+++ User_Dao/src/main/java/kr/ac/jejunu/UserDao.java
@@ -1,14 +1,15 @@
 package kr.ac.jejunu;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
-    private final ConnectionMaker connectionMaker;
+    private final DataSource dataSource;
 
-    public UserDao(ConnectionMaker connectionMaker) { this.connectionMaker = connectionMaker;}
+    public UserDao(DataSource dataSource) { this.dataSource = dataSource;}
 
     public User findById(Integer id) throws ClassNotFoundException, SQLException {
-        Connection connection = connectionMaker.getConnection();
+        Connection connection = dataSource.getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement(
                 "select * from  userinfo where id = ?"
         );
@@ -26,7 +27,7 @@ public class UserDao {
     }
 
     public void insert(User user) throws ClassNotFoundException, SQLException {
-        Connection connection = connectionMaker.getConnection();
+        Connection connection = dataSource.getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement(
                 "insert into userinfo (name, password) values ( ?, ? )"
                 , Statement.RETURN_GENERATED_KEYS
