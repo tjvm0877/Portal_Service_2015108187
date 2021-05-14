1082b4e31ce46dd408253cf4968378c6ffd5cf1c
Mr. Remark <itsmine096@gmail.com>
2021-05-15 00:54:01 +0900

    add Class(findById) + test(get)

---
+++ User_Dao/src/main/java/kr/ac/jejunu/UserDao.java
@@ -0,0 +1,29 @@
+package kr.ac.jejunu;
+
+import java.sql.*;
+
+public class UserDao {
+    public User findById(Integer id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection =
+                DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/portal_service?" +
+                                "characterEncoding=utf-8&serverTimezone=UTC"
+                        ,"jeju","1234"
+                );
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "select * from  userinfo where id = ?"
+        );
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        User user = new User();
+        user.setId(resultSet.getInt("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        return user;
+    }
+}
