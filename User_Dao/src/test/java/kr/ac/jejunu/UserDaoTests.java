1cd2480abdf9f1f3a67036c23b66f7c7fc39061a
Mr. Remark <itsmine096@gmail.com>
2021-05-15 01:04:50 +0900

    add method(insert) + test(insert)

--- User_Dao/src/test/java/kr/ac/jejunu/UserDaoTests.java
+++ User_Dao/src/test/java/kr/ac/jejunu/UserDaoTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 
 public class UserDaoTests {
@@ -19,5 +20,25 @@ public class UserDaoTests {
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
         assertThat(user.getPassword(), is(password));
+    }
+
+    @Test
+    public void insert() throws SQLException, ClassNotFoundException {
+        String name = "hyun";
+        String password = "1234";
+
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        UserDao userDao = new UserDao();
+        userDao.insert(user);
+
+        User insertedUser = userDao.findById(user.getId());
+
+        assertThat(user.getId(), greaterThan(0));
+        assertThat(insertedUser.getId(), is(user.getId()));
+        assertThat(insertedUser.getName(), is(user.getName()));
+        assertThat(insertedUser.getPassword(), is(user.getPassword()));
+
     }
 }
