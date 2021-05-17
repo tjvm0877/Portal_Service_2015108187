474ad124aa13ec6a7c574c95c8a1b3151cf3cab6
Mr. Remark <itsmine096@gmail.com>
2021-05-17 13:38:44 +0900

    Strategy(너무 많은 클래스가 생성됨) => Template Callback Pattern

--- User_Dao/src/main/java/kr/ac/jejunu/JdbcContext.java
+++ User_Dao/src/main/java/kr/ac/jejunu/JdbcContext.java
@@ -1,10 +1,7 @@
 package kr.ac.jejunu;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JdbcContext {
     private final DataSource dataSource;
