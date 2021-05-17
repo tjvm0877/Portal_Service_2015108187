62dc2788aa03ba462a4433ebed3dc807667ca6ef
Mr. Remark <itsmine096@gmail.com>
2021-05-17 13:27:29 +0900

    JdbcContext

--- User_Dao/src/main/java/kr/ac/jejunu/findByIdStatementStartegy.java
+++ User_Dao/src/main/java/kr/ac/jejunu/findByIdStatementStartegy.java
@@ -5,9 +5,13 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class findByIdStatementStartegy implements StatementStrategy {
+    private Integer id;
+    public findByIdStatementStartegy(Integer id) {
+        this.id = id;
+    }
+
     @Override
-    public PreparedStatement makeStatement(Object object, Connection connection) throws SQLException {
-        Integer id = (Integer) object;
+    public PreparedStatement makeStatement(Connection connection) throws SQLException {
         PreparedStatement preparedStatement = connection.prepareStatement(
                 "select * from  userinfo where id = ?"
         );
