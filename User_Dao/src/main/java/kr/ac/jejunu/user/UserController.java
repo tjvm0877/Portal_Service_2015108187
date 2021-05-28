db944b4ad399108d1d816e33316c877ac4e42226
Mr. Remark <itsmine096@gmail.com>
2021-05-28 18:23:40 +0900

    file upload

--- User_Dao/src/main/java/kr/ac/jejunu/user/UserController.java
+++ User_Dao/src/main/java/kr/ac/jejunu/user/UserController.java
@@ -4,8 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
 
 @Controller
 @RequestMapping
@@ -16,6 +21,23 @@ public class UserController {
     @RequestMapping("/user")
     public User getUser(@RequestParam("id") Integer id) {
         return userDao.findById(id);
+    }
+
+    @RequestMapping(path = "/upload", method = RequestMethod.GET)
+    public void upload() {
+    }
+
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public ModelAndView upload(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws IOException {
+        String path = request.getServletContext().getRealPath("/") + "/WEB-INF/static/" + file.getOriginalFilename();
+        File saveFile = new File(path);
+        FileOutputStream fileOutputStream = new FileOutputStream(saveFile);
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+        bufferedOutputStream.write(file.getBytes());
+        bufferedOutputStream.close();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("url", "/images/" + file.getOriginalFilename());
+        return modelAndView;
     }
 
     @ExceptionHandler(Exception.class)
---
+++ User_Dao/src/main/webapp/WEB-INF/views/upload.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<html>
+<h2>File Upload</h2>
+<form action="/upload" method="post" enctype="multipart/form-data">
+    <input type="file" name="file"/>
+    <input type="submit"/>
+</form>
+<img src="${url}"/>
+</html>
