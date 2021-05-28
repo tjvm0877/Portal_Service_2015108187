8b751d77de01174473943db46c56b1ea487d8243
Mr. Remark <itsmine096@gmail.com>
2021-05-28 17:09:49 +0900

    HandlerExceptionResolver

--- User_Dao/src/main/java/kr/ac/jejunu/user/UserController.java
+++ User_Dao/src/main/java/kr/ac/jejunu/user/UserController.java
@@ -2,8 +2,10 @@ package kr.ac.jejunu.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping
@@ -14,5 +16,12 @@ public class UserController {
     @RequestMapping("/user")
     public User getUser(@RequestParam("id") Integer id) {
         return userDao.findById(id);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView error(Exception e) {
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("e", e);
+        return modelAndView;
     }
 }
---
+++ User_Dao/src/main/webapp/WEB-INF/views/error.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<html>
+<h3>
+    this is error.jsp
+</h3>
+<h2>
+    Error ${e.message}
+</h2>
+</html>
